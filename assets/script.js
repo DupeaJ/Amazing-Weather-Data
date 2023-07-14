6e1637d72924c97021010fea53690c7fb4072258
DupeaJ <127266733+DupeaJ@users.noreply.github.com>
2023-07-13 21:55:16 -0700

    switch display card to display current weather from api

--- assets/script.js
+++ assets/script.js
@@ -5,15 +5,15 @@ const searchButton = document.querySelector(".search-button");
 const userInputField = document.getElementById("city-search");
 
 function callWeather() {
-const userInput = document.getElementById("city-search").value;
-    console.log(userInput)
-    
+    const userInput = document.getElementById("city-search").value;
+    console.log(userInput);
+
     localStorage.setItem("search history", userInput);
     fetch(
         url +
             "q=" +
             userInput +
-            "&units=imperial&lang=en&cnt=7&units=standard" +
+            "&units=imperial&lang=en&cnt=5&units=standard" +
             APIKey
     )
         .then(function (response) {
@@ -21,25 +21,18 @@ const userInput = document.getElementById("city-search").value;
         })
 
         .then(function (data) {
-            
             const name = data.city.name;
             const temp = data.list[0].main.temp;
             const speed = data.list[0].wind.speed;
             const humidity = data.list[0].main.humidity;
             const date = data.list[0].dt_txt;
-            const dt = data.list[2].dt
+            const dt = data.list[4].dt;
             console.log(data);
-            console.log(name);
-            console.log(temp);
+            console.log(dt);
+
             
-            
-            $("#city-name").text(name + " (" + date + ")");
-            
-            $("#temperature").text("Temperature: " + temp + "°");
-            $("#wind-speed").text("Wind Speed: " + speed + " mph");
-            $("#humidity").text("Humidity: " + humidity + " %");
             $("#forcast-day-1").text(dayjs.unix(dt).format("M/D/YY"));
-            
+
             const icon = data.list[0].weather[0].icon;
             fetch("https://openweathermap.org/img/wn/" + icon + "@2x.png")
                 .then(function (response) {
@@ -49,8 +42,32 @@ const userInput = document.getElementById("city-search").value;
                     const weatherIconURL = URL.createObjectURL(blob);
                     document.getElementById("weather-icon").src =
                         weatherIconURL;
-                });
-           
+                })
+                fetch(
+                    "https://api.openweathermap.org/data/2.5/weather?q=" +
+                        userInput +
+                    "&units=imperial&lang=en&cnt=5&units=standard" +
+                    APIKey
+                )
+                    .then(function (response) {
+                        return response.json();
+                    })
+                    .then(function (data) {
+                        console.log(data);
+                        const name = data.name;
+                        const temp = data.main.temp;
+                        const speed = data.wind.speed;
+                        const humidity = data.main.humidity;
+                        const dt = data.dt;
+
+                        $("#city-name").text(name+" (" +dayjs.unix(dt).format("M/D/YY")+")");;
+                        $("#temperature").text("Temperature: " + temp + "°");
+                        $("#wind-speed").text("Wind Speed: " + speed + " mph");
+                        $("#humidity").text("Humidity: " + humidity + " %");
+
+
+            })
+
             console.log(speed);
             console.log(humidity);
         })
@@ -68,7 +85,7 @@ function displayBtn() {
         const search = document.getElementById("search-history-container");
         const scoreBtn = document.createElement("button");
         scoreBtn.textContent = searchHistory;
-        search.appendChild(scoreBtn)
+        search.appendChild(scoreBtn);
     }
 }
 
