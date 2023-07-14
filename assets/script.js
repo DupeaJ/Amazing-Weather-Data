26c53d19ef04e360e0a5d17f4869e859601e2307
DupeaJ <127266733+DupeaJ@users.noreply.github.com>
2023-07-14 00:22:58 -0700

    finshed local storage and recal with buttons

--- assets/script.js
+++ assets/script.js
@@ -6,9 +6,17 @@ const userInputField = document.getElementById("city-search");
 
 function callWeather() {
     const userInput = document.getElementById("city-search").value;
-    console.log(userInput);
+    //console.log(userInput);
 
-    localStorage.setItem("search history", userInput);
+    let searchHistory = localStorage.getItem("searchHistory");
+    if (searchHistory) {
+        searchHistory = JSON.parse(searchHistory);
+    } else {
+        searchHistory = [];
+    }
+    searchHistory.push(userInput);
+    localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
+
     fetch(
         url +
         "q=" +
@@ -21,12 +29,12 @@ function callWeather() {
         })
 
         .then(function (data) {
-            console.log(data);
+            //console.log(data);
             // console.log(dt);
             for (let i = 7; i < data.list.length; i += 7) {
-                 const temp = data.list[i].main.temp;
-                 const speed = data.list[i].wind.speed;
-                 const humidity = data.list[i].main.humidity;
+                const temp = data.list[i].main.temp;
+                const speed = data.list[i].wind.speed;
+                const humidity = data.list[i].main.humidity;
                 const dt = data.list[i].dt;
                 const formattedDate = dayjs.unix(dt).format("M/D/YY");
                 const elementId = "#forcast-day-" + Math.ceil((i + 0) / 7);
@@ -34,13 +42,13 @@ function callWeather() {
             
             
                 const tempElementId = "#temp-day-" + Math.ceil((i + 0) / 7);
-                    $(tempElementId).text(temp)
+                    $(tempElementId).text("Temp: " + temp + "°");
                     
                 const speedElementId = "#wind-day-" + Math.ceil((i + 0) / 7);
-                $(speedElementId).text(speed)
+                $(speedElementId).text("Speed: " + speed + "mph");
                 
                 const humidityElementId = "#humidity-day-" + Math.ceil((i + 0) / 7);
-                $(humidityElementId).text(humidity)
+                $(humidityElementId).text("Humidity: " + humidity + "%");
 
                 const icon = data.list[i].weather[0].icon;
                 const iconElementId = "#icon-day-" + Math.ceil((i + 0) / 7);
@@ -65,7 +73,7 @@ function callWeather() {
                     return response.json();
                 })
                 .then(function (data) {
-                    console.log(data);
+                    //console.log(data);
                     const name = data.name;
                     const temp = data.main.temp;
                     const speed = data.wind.speed;
@@ -97,21 +105,139 @@ function callWeather() {
         })
 
         .catch((error) => {
-            console.log(error);
+            //console.log(error);
         });
     displayBtn();
 }
 
 function displayBtn() {
-    const searchHistory = localStorage.getItem("search history");
-    console.log(searchHistory);
+    const searchHistory = localStorage.getItem("searchHistory");
+    //console.log(searchHistory);
     if (searchHistory) {
-        const search = document.getElementById("search-history-container");
-        const scoreBtn = document.createElement("button");
-        scoreBtn.textContent = searchHistory;
-        search.appendChild(scoreBtn);
+        const searchHistoryArray = JSON.parse(searchHistory);
+        const searchContainer = document.getElementById(
+            "search-history-container"
+        );
+        for (let i = 0; i < searchHistoryArray.length; i++) {
+            const searchItem = searchHistoryArray[i];
+            if (!isItemDisplayed(searchItem, searchContainer)) {
+                const scoreBtn = document.createElement("button");
+                scoreBtn.textContent = searchItem;
+                searchContainer.appendChild(scoreBtn);
+
+                scoreBtn.addEventListener("click", function () {
+                    history(searchItem);
+                });
+            }
+        }
     }
 }
+
+function history(searchItem) {
+fetch(
+        url +
+        "q=" +
+        searchItem +
+        "&units=imperial&lang=en&units=standard" +
+        APIKey
+    )
+        .then(function (response) {
+            return response.json();
+        })
+
+        .then(function (data) {
+            //(data);
+            // console.log(dt);
+            for (let i = 7; i < data.list.length; i += 7) {
+                const temp = data.list[i].main.temp;
+                const speed = data.list[i].wind.speed;
+                const humidity = data.list[i].main.humidity;
+                const dt = data.list[i].dt;
+                const formattedDate = dayjs.unix(dt).format("M/D/YY");
+                const elementId = "#forcast-day-" + Math.ceil((i + 0) / 7);
+                    $(elementId).text(formattedDate)
+            
+            
+                const tempElementId = "#temp-day-" + Math.ceil((i + 0) / 7);
+                    $(tempElementId).text("Temp: " + temp + "°");
+                    
+                const speedElementId = "#wind-day-" + Math.ceil((i + 0) / 7);
+                $(speedElementId).text("Speed: " + speed + "mph");
+                
+                const humidityElementId = "#humidity-day-" + Math.ceil((i + 0) / 7);
+                $(humidityElementId).text("Humidity: " + humidity + "%");
+
+                const icon = data.list[i].weather[0].icon;
+                const iconElementId = "#icon-day-" + Math.ceil((i + 0) / 7);
+
+                fetch("https://openweathermap.org/img/wn/" + icon + "@2x.png")
+                .then(function (response) {
+                    return response.blob();
+                })
+                .then(function (blob) {
+                    const weatherIconURL = URL.createObjectURL(blob);
+                    $(iconElementId).attr("src", weatherIconURL);
+                });
+            };
+            //Current weather fetch
+            fetch(
+                "https://api.openweathermap.org/data/2.5/weather?q=" +
+                    searchItem +
+                    "&units=imperial&lang=en&cnt=5&units=standard" +
+                    APIKey
+            )
+                .then(function (response) {
+                    return response.json();
+                })
+                .then(function (data) {
+                    //console.log(data);
+                    const name = data.name;
+                    const temp = data.main.temp;
+                    const speed = data.wind.speed;
+                    const humidity = data.main.humidity;
+                    const dt = data.dt;
+                    const icon = data.weather[0].icon
+
+                    $("#city-name").text(
+                        name + " (" + dayjs.unix(dt).format("M/D/YY") + ")"
+                    );
+                    $("#temperature").text("Temperature: " + temp + "°");
+                    $("#wind-speed").text("Wind Speed: " + speed + " mph");
+                    $("#humidity").text("Humidity: " + humidity + " %");
+                    // const icon = weather[0].icon;
+                    const weatherI = "#weather-icon"
+                    fetch(
+                        "https://openweathermap.org/img/wn/" + icon + "@2x.png"
+                    )
+                        .then(function (response) {
+                            return response.blob();
+                        })
+                        .then(function (blob) {
+                            const weatherIconURL = URL.createObjectURL(blob);
+                            $(weatherI).attr("src", weatherIconURL);
+                        });
+                });
+        
+            
+        })
+
+        .catch((error) => {
+            console.log(error);
+        });
+}
+displayBtn();
+
+
+function isItemDisplayed(item, container) {
+    const buttons = container.getElementsByTagName("button");
+    for (let i = 0; i < buttons.length; i++) {
+        if (buttons[i].textContent === item) {
+            return true;
+        }
+    }
+    return false;
+}
+
 
 searchButton.addEventListener("click", callWeather);
 userInputField.addEventListener("keydown", function (event) {
